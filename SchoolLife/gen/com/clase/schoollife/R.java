576027a9372d29a89f36d3eb11a239e5fb328d62
José Santos Sánchez Sánchez <js_bailen@hotmail.com>
2013-04-23 11:16:27 +0200

    Comienzo New Task

--- SchoolLife/gen/com/clase/schoollife/R.java
+++ SchoolLife/gen/com/clase/schoollife/R.java
@@ -30,18 +30,18 @@ public final class R {
     }
     public static final class id {
         public static final int button1=0x7f090009;
-        public static final int editText1=0x7f090004;
-        public static final int editText2=0x7f090002;
-        public static final int editText3=0x7f090003;
-        public static final int editText4=0x7f090006;
+        public static final int editText1=0x7f090003;
+        public static final int editText2=0x7f090006;
+        public static final int editText3=0x7f090007;
+        public static final int editText4=0x7f090008;
         public static final int menu_add=0x7f09000a;
         public static final int menu_help=0x7f09000c;
         public static final int settings=0x7f09000b;
-        public static final int textView1=0x7f090005;
-        public static final int textView2=0x7f090007;
-        public static final int textView3=0x7f090000;
-        public static final int textView4=0x7f090001;
-        public static final int textView5=0x7f090008;
+        public static final int textView1=0x7f090001;
+        public static final int textView2=0x7f090002;
+        public static final int textView3=0x7f090004;
+        public static final int textView4=0x7f090005;
+        public static final int textView5=0x7f090000;
     }
     public static final class layout {
         public static final int activity_create=0x7f030000;
@@ -57,15 +57,25 @@ public final class R {
         public static final int add=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int classroom=0x7f050006;
+        public static final int completed=0x7f050013;
         public static final int confirm_button=0x7f05000b;
-        public static final int delete=0x7f05000c;
+        public static final int date=0x7f05000f;
+        public static final int delete=0x7f050016;
         public static final int edit=0x7f050004;
+        public static final int explanation=0x7f05000e;
+        public static final int feelings=0x7f050014;
         public static final int help=0x7f050005;
+        public static final int mark=0x7f050010;
         public static final int new_subject_title=0x7f05000a;
+        public static final int new_task_title=0x7f050015;
         public static final int no_subject=0x7f050003;
         public static final int professor=0x7f050008;
+        public static final int revision=0x7f050011;
+        public static final int revision_date=0x7f050012;
         public static final int settings=0x7f050001;
         public static final int subject=0x7f050007;
+        public static final int task=0x7f05000c;
+        public static final int title=0x7f05000d;
     }
     public static final class style {
         /** 
