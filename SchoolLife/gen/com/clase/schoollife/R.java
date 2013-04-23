7520b7e61e40cf94e5c85fbf0e4cbaf4baa78cf7
José Santos Sánchez Sánchez <js_bailen@hotmail.com>
2013-04-23 10:19:30 +0200

    Abbreviation

--- SchoolLife/gen/com/clase/schoollife/R.java
+++ SchoolLife/gen/com/clase/schoollife/R.java
@@ -53,7 +53,7 @@ public final class R {
         public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int abreviature=0x7f050009;
+        public static final int abbreviation=0x7f050009;
         public static final int add=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int classroom=0x7f050006;
