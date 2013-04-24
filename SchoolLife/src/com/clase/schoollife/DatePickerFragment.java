4a599f489a737ecab29ccb85795428323b9d501d
Daniel Sánchez Seijo <daniseijo12@gmail.com>
2013-04-24 17:49:41 +0200

    DatePicker

---
+++ SchoolLife/src/com/clase/schoollife/DatePickerFragment.java
@@ -0,0 +1,35 @@
+package com.clase.schoollife;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+public class DatePickerFragment extends DialogFragment
+	implements DatePickerDialog.OnDateSetListener {
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		// Use the current date as the default date in the picker
+		final Calendar c = Calendar.getInstance();
+		int year = c.get(Calendar.YEAR);
+		int month = c.get(Calendar.MONTH);
+		int day = c.get(Calendar.DAY_OF_MONTH);
+
+		// Create a new instance of DatePickerDialog and return it
+		return new DatePickerDialog(getActivity(), this, year, month, day);
+	}
+	
+	@Override
+	public void onDateSet(DatePicker view, int year, int month, int day) {
+		TextView fecha = (TextView) getActivity().findViewById(R.id.textView6);
+		Calendar myDate = new GregorianCalendar(year, month, day);
+		fecha.setText(DateFormat.getDateInstance().format(myDate.getTime()));
+	}
+}
